c0cec6b42dc6d435ca76566ebc351ac25f981aba
loscabezones <david_gaia2@hotmail.com>
2019-01-15 18:12:14 +0100

    creacion de subcomponente de autochat service

---
+++ src/app/components/autochatoutput/autochatoutput.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autochatoutput',
+  templateUrl: './autochatoutput.component.html',
+  styleUrls: ['./autochatoutput.component.scss']
+})
+export class AutochatoutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/autochatwithservice/autochatwithservice.component.ts
+++ src/app/components/autochatwithservice/autochatwithservice.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutochatwithserviceComponent implements OnInit {
 
+  conversation:Array<string> = [];
+  defaultText:string = 'Mensaje';
+  model = { message: ''};
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit() {
+
+    let text = this.model.message
+
+    //Guardamos el mensaje introducido en un array para mostrarlo en el chat
+    if(text.length >0){
+      this.conversation.push(text);
+    }
+    console.log(this.conversation);
+
+    //eliminamos el texto introducido en el input text//
+    //eliminamos el valor del placeHolder
+    this.model.message = '';
+    this.defaultText = '';
+
   }
 
 }
