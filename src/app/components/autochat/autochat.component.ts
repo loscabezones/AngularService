ec8140e4c74f41e63ec9b9a7820d10838151c103
loscabezones <david_gaia2@hotmail.com>
2019-01-15 17:20:54 +0100

    autochat basico completado

--- src/app/components/autochat/autochat.component.ts
+++ src/app/components/autochat/autochat.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutochatComponent implements OnInit {
 
+  conversation:Array<string> = [];
   defaultText:string = 'Mensaje';
   model = { message: ''};
 
@@ -15,8 +16,19 @@ export class AutochatComponent implements OnInit {
   ngOnInit() {}
 
   onSubmit() {
-    console.log(this.model.message)
+
+    let text = this.model.message
+
+    //Guardamos el mensaje introducido en un array para mostrarlo en el chat
+    if(text.length >0){
+      this.conversation.push(text);
+    }
+    console.log(this.conversation);
+
+    //eliminamos el texto introducido en el input text//
+    //eliminamos el valor del placeHolder
     this.model.message = '';
     this.defaultText = '';
+
   }
 }
