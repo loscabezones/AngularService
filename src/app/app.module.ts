b88de8f52b6490732129ef40e100368d733c35f5
loscabezones <david_gaia2@hotmail.com>
2019-01-16 17:20:25 +0100

    Creado componente profilemenu, y puesto en el header, convertido en dropdawn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AutochatComponent } from './components/autochat/autochat.component';
 import { AutochatwithserviceComponent } from './components/autochatwithservice/autochatwithservice.component';
 import { AutochatinputComponent } from './components/autochatinput/autochatinput.component';
 import { AutochatoutputComponent } from './components/autochatoutput/autochatoutput.component';
+import { ProfilemenuComponent } from './components/profilemenu/profilemenu.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { AutochatoutputComponent } from './components/autochatoutput/autochatout
     AutochatComponent,
     AutochatwithserviceComponent,
     AutochatinputComponent,
-    AutochatoutputComponent
+    AutochatoutputComponent,
+    ProfilemenuComponent
   ],
   imports: [
     FormsModule,
