f85fe051b767d3ccd7e8a62a6d1e428eec85ed05
loscabezones <david_gaia2@hotmail.com>
2019-01-16 15:25:05 +0100

    creacion de componente autochatwithservice que tuliza dos componentes, el autochatinput y el autochat output

--- src/app/components/autochatinput/autochatinput.component.ts
+++ src/app/components/autochatinput/autochatinput.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-autochatinput',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutochatinputComponent implements OnInit {
 
-  constructor() { }
+  conversation:Array<string> = [];
+  defaultText:string = 'Mensaje';
+  model = { message: ''};
+
+  constructor( private mainService : MainService) {
+      // guardamos el valor que devueve getConversation en nuestro array
+        this.conversation = this.mainService.getConversation();
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+
+    let text = this.model.message
+
+    //Guardamos el mensaje introducido en el array
+    if(text.length >0){
+      this.conversation.push(text);
+    }
+
+    //eliminamos el texto introducido en el input text//
+    //eliminamos el valor del placeHolder
+    this.model.message = '';
+    this.defaultText = '';
+
   }
 
 }
--- src/app/components/autochatoutput/autochatoutput.component.ts
+++ src/app/components/autochatoutput/autochatoutput.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-autochatoutput',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autochatoutput.component.scss']
 })
 export class AutochatoutputComponent implements OnInit {
+  
+  conversation:Array<string> = [];
 
-  constructor() { }
+
+  constructor( private mainService: MainService) {
+    //guardamos el valor que devuelve getConversation en nuestro array//
+      this.conversation = this.mainService.getConversation();
+   }
 
   ngOnInit() {
   }
